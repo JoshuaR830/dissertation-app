40af8bc0fd3fca9bd41b2fe10dce6dbc512cbc27
Joshua <psyjr4@nottingham.ac.uk>
2020-12-25 07:41:01 +0000

    Menu items added

--- app/src/main/java/com/joshuarichardson/fivewaystowellbeing/MainActivity.java
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/MainActivity.java
@@ -1,12 +1,16 @@
 package com.joshuarichardson.fivewaystowellbeing;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.help_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.menu_settings:
+                Log.d("Menu", "Settings");
+                break;
+            case R.id.menu_wellbeing_support:
+                Log.d("Menu", "Get wellbeing support");
+                break;
+            default:
+                Log.d("Menu", "Menu");
+        }
+
+        return true;
     }
 
     public void onButtonClick(View v) {
