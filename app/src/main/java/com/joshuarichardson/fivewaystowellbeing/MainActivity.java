13af945bfe6565a156ae2f338aa21acb8434a9a5
Joshua <psyjr4@nottingham.ac.uk>
2021-01-05 20:31:36 +0000

    WIP - started adding a new fragment for settings

--- app/src/main/java/com/joshuarichardson/fivewaystowellbeing/MainActivity.java
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/MainActivity.java
@@ -11,14 +11,18 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        this.fragmentManager = getSupportFragmentManager();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -35,12 +39,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        WellbeingSupportFragment supportFragment = new WellbeingSupportFragment();
+        SettingsFragment settingsFragment = new SettingsFragment();
+
         switch(item.getItemId()) {
             case R.id.menu_settings:
                 Log.d("Menu", "Settings");
+                this.fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, supportFragment, null)
+                        .addToBackStack(null)
+                        .commit();
                 break;
             case R.id.menu_wellbeing_support:
                 Log.d("Menu", "Get wellbeing support");
+                this.fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, settingsFragment, null)
+                        .addToBackStack(null)
+                        .commit();
                 break;
             default:
                 Log.d("Menu", "Menu");
---
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/WellbeingSupportFragment.java
@@ -0,0 +1,6 @@
+package com.joshuarichardson.fivewaystowellbeing;
+
+import androidx.fragment.app.Fragment;
+
+public class WellbeingSupportFragment extends Fragment {
+}
