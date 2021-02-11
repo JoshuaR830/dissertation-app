f94595c01f8fcd31b3b1090844d1464ecff575eb
Joshua <psyjr4@nottingham.ac.uk>
2021-02-11 11:28:34 +0000

    Added migration back

--- app/src/main/java/com/joshuarichardson/fivewaystowellbeing/hilt/modules/WellbeingDatabaseModule.java
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/hilt/modules/WellbeingDatabaseModule.java
@@ -41,11 +41,76 @@ public class WellbeingDatabaseModule {
         }
     };
 
+    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+
+            // Create the new questions table
+            database.execSQL("CREATE TABLE wellbeing_questions (" +
+                "wellbeing_question_id INTEGER NOT NULL PRIMARY KEY, " +
+                "question TEXT NOT NULL, " +
+                "positive_message TEXT NOT NULL, " +
+                "negative_message TEXT NOT NULL, " +
+                "way_to_wellbeing TEXT NOT NULL, " +
+                "weighting INTEGER NOT NULL, " +
+                "activity_type TEXT NOT NULL, " +
+                "input_type TEXT NOT NULL " +
+                ")"
+            );
+
+            // Create the new wellbeing records table
+            database.execSQL("CREATE TABLE wellbeing_records (" +
+                "wellbeing_record_id INTEGER NOT NULL PRIMARY KEY, " +
+                    "time INTEGER NOT NULL, " +
+                    "user_input INTEGER NOT NULL default 0, " +
+                    "survey_response_activity_record_id INTEGER NOT NULL, " +
+                    "sequence_number INTEGER NOT NULL default 0, " +
+                    "question_id INTEGER NOT NULL, "  +
+                    "FOREIGN KEY(question_id) REFERENCES wellbeing_questions(wellbeing_question_id) ON DELETE CASCADE, " +
+                    "FOREIGN KEY(survey_response_activity_record_id) REFERENCES survey_activity(survey_activity_id) ON DELETE CASCADE" +
+                    ")"
+            );
+
+            // Create a new table that will replace the survey_activity table - this is required because old table has the wrong primary key
+            // Reference: medium.com/@pekwerike/handling-roomdb-migration-create-a-new-primary-key-column-in-an-existing-entity-f15d10932f5b
+            database.execSQL("CREATE TABLE new_survey_activity (" +
+                    "survey_activity_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                    "survey_response_id INTEGER NOT NULL, " +
+                    "activity_record_id INTEGER NOT NULL, " +
+                    "sequence_number INTEGER NOT NULL, " +
+                    "note TEXT, " +
+                    "start_time INTEGER NOT NULL, " +
+                    "end_time INTEGER NOT NULL, " +
+                    "FOREIGN KEY(activity_record_id) REFERENCES activity_records(id) ON DELETE CASCADE, " +
+                    "FOREIGN KEY(survey_response_id) REFERENCES survey_response(id) ON DELETE CASCADE" +
+                ");"
+            );
+
+            // Insert the values from the old table and new default values into the new table
+            database.execSQL("INSERT INTO new_survey_activity(" +
+                    "survey_response_id, " +
+                    "activity_record_id, " +
+                    "sequence_number, " +
+                    "note, " +
+                    "start_time, " +
+                    "end_time " +
+                ")" +
+                "SELECT survey_response_id, activity_record_id, 0, null, -1, -1 FROM survey_activity"
+            );
+
+            // Delete the old table
+            database.execSQL("DROP TABLE survey_activity");
+
+            // Rename the new table
+            database.execSQL("ALTER TABLE new_survey_activity RENAME TO survey_activity");
+        }
+    };
+
     @Provides
     @Singleton
     public static WellbeingDatabase getWellbeingDatabase(@ApplicationContext Context context) {
         return Room.databaseBuilder(context, WellbeingDatabase.class, WELLBEING_DATABASE_NAME)
-            .addMigrations(MIGRATION_1_2)
+            .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
             .addCallback(new RoomDatabase.Callback() {
                 @Override
                 public void onCreate(@NonNull SupportSQLiteDatabase db) {
