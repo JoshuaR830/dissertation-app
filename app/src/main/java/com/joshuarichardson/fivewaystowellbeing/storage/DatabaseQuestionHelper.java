18159fd0c3745f7ca23819204bc25433c9f5bb40
Joshua <psyjr4@nottingham.ac.uk>
2021-02-15 08:24:22 +0000

    Fixed the system that adds the questions to the database

--- app/src/main/java/com/joshuarichardson/fivewaystowellbeing/storage/DatabaseQuestionHelper.java
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/storage/DatabaseQuestionHelper.java
@@ -7,6 +7,8 @@ import com.joshuarichardson.fivewaystowellbeing.surveys.SurveyItemTypes;
 
 public class DatabaseQuestionHelper {
 
+    public static final int VERSION_NUMBER = 1;
+
     public static final int ACTIVITY_OF_TYPE_VALUE = 50;
 
     private static final int HIGH_VALUE = 20;
