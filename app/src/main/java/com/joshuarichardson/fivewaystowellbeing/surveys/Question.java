9178f13a891cba620f186a3a579b9647f522ccab
Joshua <psyjr4@nottingham.ac.uk>
2021-02-16 19:29:31 +0000

    Removed some extra code that were left over

--- app/src/main/java/com/joshuarichardson/fivewaystowellbeing/surveys/Question.java
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/surveys/Question.java
@@ -4,7 +4,6 @@ public class Question {
     private String question;
     private long wellbeingRecordId;
     private Boolean userResponse;
-    private String wayToWellbeing;
 
     public Question(String question, long wellbeingRecordId, Boolean userResponse) {
         this.question = question;
