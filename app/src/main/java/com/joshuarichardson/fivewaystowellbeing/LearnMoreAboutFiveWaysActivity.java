e6a6fed403e64c3b5c86491da6b8c4319ad47968
Joshua <psyjr4@nottingham.ac.uk>
2021-02-19 16:13:31 +0000

    Fixed info bug and added tap on item to expand and changed some colours

--- app/src/main/java/com/joshuarichardson/fivewaystowellbeing/LearnMoreAboutFiveWaysActivity.java
+++ app/src/main/java/com/joshuarichardson/fivewaystowellbeing/LearnMoreAboutFiveWaysActivity.java
@@ -1,6 +1,9 @@
 package com.joshuarichardson.fivewaystowellbeing;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,11 @@ public class LearnMoreAboutFiveWaysActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_more_about_five_ways);
+    }
+
+    public void onLearnMoreButtonClicked(View v) {
+        // Open research PDF in Chrome
+        Intent webViewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://neweconomics.org/uploads/files/five-ways-to-wellbeing-1.pdf"));
+        startActivity(webViewIntent);
     }
 }
